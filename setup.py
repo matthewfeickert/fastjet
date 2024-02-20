46e49d63daad5f5bbebf057fd30dbe61fb690712
Matthew Feickert <matthew.feickert@cern.ch>
2024-02-19 23:04:30 -0600

    Remove manual insertion of CGAL

--- setup.py
+++ setup.py
@@ -49,16 +49,6 @@ def get_version() -> str:
 class FastJetBuild(setuptools.command.build_ext.build_ext):
     def build_extensions(self):
         if not OUTPUT.exists():
-            zip_filename = DIR / pathlib.Path(CGAL_ZIP).parts[-1]
-
-            with urllib.request.urlopen(CGAL_ZIP) as http_obj:
-                with open(zip_filename, "wb") as file_obj:
-                    shutil.copyfileobj(http_obj, file_obj)
-
-            with zipfile.ZipFile(zip_filename) as zip_obj:
-                cgal_dir = DIR / zip_obj.namelist()[0]
-                zip_obj.extractall(DIR)
-
             # Patch for segfault of LimitedWarning
             # For more info see https://github.com/scikit-hep/fastjet/pull/131
             subprocess.run(
@@ -86,7 +76,6 @@ class FastJetBuild(setuptools.command.build_ext.build_ext):
                 "--enable-allcxxplugins",
                 "--enable-cgal-header-only",
                 "--enable-cgal",
-                f"--with-cgaldir={cgal_dir}",
                 "--enable-swig",
                 "--enable-pyext",
                 f'LDFLAGS={env["LDFLAGS"]}',
